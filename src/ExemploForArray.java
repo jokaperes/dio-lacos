7b47de954420683b4990ef964c11a0d5f01dc52d
jokaperes <jokaperes@gmail.com>
2023-09-05 20:39:35 -0400

    inicio

---
+++ src/ExemploForArray.java
@@ -0,0 +1,11 @@
+// ExemploFor.java
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String[] alunos = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int  i= 0; i <alunos.length; i++) {
+            System.out.println("O aluno no indice i = " + i + " é " + alunos[i]);
+        }
+
+    }
+}
